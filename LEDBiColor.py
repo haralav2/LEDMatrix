2a01c26bb66d555db78e31d45b53ae117ea0e8f5
Veneta <haralav2@cs.man.ac.uk>
2013-07-13 12:44:34 +0000

    Text done

--- LEDBiColor.py
+++ LEDBiColor.py
@@ -7,10 +7,6 @@ import smbus
 import time
 import sys
 import font8x8
-import cProfile, pstats, io
-
-
-pr = cProfile.Profile()
 
 
 # Definitions for the MCP23017
@@ -25,8 +21,9 @@ RED = 1
 GREEN = 2
 YELLOW = 3
 ledColor = 1
+
+# Set all 16 pins to be output
 def initialise():
-    # Set all 16 pins to be output
     bus.write_byte_data(ADDRR,DIRA,0x00)   # All outputs on PortA address 0x20
     bus.write_byte_data(ADDRR,DIRB,0x00)   # All outputs on PortB address 0x20
     bus.write_byte_data(ADDRC,DIRA,0x00)   # All outputs on PortA address 0x21 - green
@@ -37,8 +34,8 @@ def initialise():
 
 
 
+# Turn on one column with specific color - either green,red or yellow
 def turnOnColumn(color,column):
-    # Turn on one column with specific color - either green,red or yellow
     if color == RED:
         bus.write_byte_data(ADDRC,PORTB,~(1<<column))
         bus.write_byte_data(ADDRC,PORTA,0xFF)
@@ -50,8 +47,8 @@ def turnOnColumn(color,column):
         bus.write_byte_data(ADDRC,PORTA,~(1<<column))
     bus.write_byte_data(ADDRR,PORTA,0xFF)
 
-def turnOnRow(color,row):
-    # Turn on one row with specific color - either green,red or yellow    
+# Turn on one row with specific color - either green,red or yellow 
+def turnOnRow(color,row):   
     if color == RED:
         bus.write_byte_data(ADDRC,PORTA,0xFF)
         bus.write_byte_data(ADDRC,PORTB,0x00) 
@@ -64,9 +61,8 @@ def turnOnRow(color,row):
     bus.write_byte_data(ADDRR,PORTA,1<<row)
     
 
-
+# Turn on individual LED with a specific color - green,red ot yellow
 def turnOnLed(color,row,column):
-    # Turn on individual LED with a specific color - green,red ot yellow
     if color == RED:
         bus.write_byte_data(ADDRC,PORTB,~(1<<column))
         bus.write_byte_data(ADDRC,PORTA,0xFF)
@@ -79,8 +75,8 @@ def turnOnLed(color,row,column):
     bus.write_byte_data(ADDRR,PORTA,1<<row)
     
 
+# Turn on all LEDs
 def turnOnAll(color):
-    # Turn on all LEDs
     if color == RED:
         bus.write_byte_data(ADDRC,PORTA,0xFF)
         bus.write_byte_data(ADDRC,PORTB,0x00)
@@ -92,14 +88,15 @@ def turnOnAll(color):
         bus.write_byte_data(ADDRC,PORTA,0x00)    
     bus.write_byte_data(ADDRR,PORTA,0xFF)
 
+# Turn off all LEDs
 def turnOffAll():
-    # Turn off all LEDs
     bus.write_byte_data(ADDRR,PORTA,0x00)
     bus.write_byte_data(ADDRC,PORTB,0x00)
     bus.write_byte_data(ADDRC,PORTA,0x00)
-    
-def turnOffLed(color,row,column):
-    # Turn off individual LED with a specific color - green,red or yellow
+
+
+# Turn off individual LED with a specific color - green,red or yellow    
+def turnOffLed(color,row,column):    
     if color == RED:
         bus.write_byte_data(ADDRC,PORTB,~(1<<column))
     elif color == GREEN:
@@ -109,9 +106,9 @@ def turnOffLed(color,row,column):
         bus.write_byte_data(ADDRC,PORTA,~(1<<column))
     bus.write_byte_data(ADDRR,PORTA,1<<row)
     
-    
-def turnOnLeds(color,row,column):
-    # Turn on individual LED with a specific color - green,red or yellow
+
+# Turn on LEDs with a specific color - green,red or yellow    
+def turnOnLeds(color,row,column):   
     if color == RED:
         bus.write_byte_data(ADDRC,PORTA,0xFF)
         bus.write_byte_data(ADDRC,PORTB,~column)
@@ -123,36 +120,36 @@ def turnOnLeds(color,row,column):
         bus.write_byte_data(ADDRC,PORTA,~column)
     bus.write_byte_data(ADDRR,PORTA,1<<row)
 
-def multiplexing(patternGreen,patternRed,patternYellow,count):
-    pr.enable()
-    for count in range(0,count):
-        for row in range(0,8):
-            turnOnLeds(GREEN,row,patternGreen[row])
-            turnOnLeds(RED,row,patternRed[row])
-            turnOnLeds(YELLOW,row,patternYellow[row])
-            time.sleep(0.001)
-            #turnOffAll()
-    pr.disable()
-    #s = io.StringIO()
-    pstats.Stats(pr).print_stats()
 
+# Going through all the rows quickly to create the illusion of a static image
 def multiplexingText(color,pattern,count):
-    if color == GREEN:
+    if color == "2\n" or color == "green\n" or color == "2" or color == "green":
         for count in range(0,count):
             for row in range(0,8):
-                turnOnLeds(GREEN,row,pattern[row])
-                time.sleep(0.0003)
-    elif color == YELLOW:
+                bus.write_byte_data(ADDRC,PORTB,0xFF)
+                bus.write_byte_data(ADDRC,PORTA,~pattern[row])
+                bus.write_byte_data(ADDRR,PORTA,1<<row)                
+                turnOffAll()
+    elif color == "3\n" or color == "yellow\n" or color == "3" or color == "yellow":
         for count in range(0,count):
             for row in range(0,8):
-                turnOnLeds(YELLOW,row,pattern[row])
-                time.sleep(0.0003)
+                bus.write_byte_data(ADDRC,PORTA,~pattern[row])
+                bus.write_byte_data(ADDRC,PORTB,~pattern[row])
+                bus.write_byte_data(ADDRR,PORTA,1<<row)
+                turnOffAll()
     else:
         for count in range(0,count):
             for row in range(0,8):
-                turnOnLeds(RED,row,pattern[row])
-                time.sleep(0.0003)
+                bus.write_byte_data(ADDRC,PORTA,0xFF)
+                bus.write_byte_data(ADDRC,PORTB,~pattern[row])
+                bus.write_byte_data(ADDRR,PORTA,1<<row)                
+                turnOffAll()
+                
+    
+    time.sleep(0.0002)
 
+
+# Multiplexing images
 def fastMultiplexing(patternGreen,patternRed,patternYellow,count):
     for count in range(0,count):
         for row in range(0,8):
@@ -162,20 +159,9 @@ def fastMultiplexing(patternGreen,patternRed,patternYellow,count):
             redOrYellow = patternRed[row]|patternYellow[row]
             bus.write_byte_data(ADDRC,PORTB,~redOrYellow)
             bus.write_byte_data(ADDRR,PORTA,1<<row)
-            time.sleep(0.0012)
-                
-def displayText():
-    color = raw_input("Choose the color of your text - green,red or yellow")
-    text = raw_input("Enter some text to display:")
-    for char in text:
-        pattern = font8x8.font8x8[ord(char)]
-        multiplexingText(color,pattern,100)
+            time.sleep(0.0002)
 
-def charImage(character):
-    index = ord(character)
-    image = list(font8x8.font8x8[index])
-    return image
-
+# Scrolling a pattern
 def scroll(pattern,bufferText):
     for row in range(0,8):
         pattern[row] >>= 1
@@ -183,21 +169,37 @@ def scroll(pattern,bufferText):
             pattern[row] |= 0x80
         bufferText[row] >>= 1
 
+
+# Using the font display the text on the matrix
 def textScroll(ledColor,text):
-    speed = 10
+    speed = 8
     pattern = [0,0,0,0,0,0,0,0]
-    for char in text:
-        bufferText = charImage(char)
-        for shiftCount in range(0,8):
+    for character in text:
+        bufferText = list(font8x8.font8x8[ord(character)])
+        for count in range(0,8):
             multiplexingText(ledColor,pattern,speed)
             scroll(pattern,bufferText)
 
 
-
+################################# Start test ################################
 
 bus = smbus.SMBus(1)
 initialise()
 
+patternRed = [ 0x18, 0x3C, 0x3C, 0x18, 0x18, 0x00, 0x18, 0x00]
+patternYellow = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
+patternGreen = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
+fastMultiplexing(patternGreen,patternRed,patternYellow,50)
+
+# Greet the user 
+greeting = "Hello!What's your name?\n"
+print greeting
+textScroll(ledColor,greeting)
+
+name = sys.stdin.read()
+nameAndGreeting = "Hello " + name + "I am PiLight!"
+print nameAndGreeting
+textScroll(ledColor,nameAndGreeting)
 
 number = 0
 while (number < 10):
@@ -287,15 +289,25 @@ patternRed = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
 patternYellow = [0x00,0x00,0x18,0x3C,0x3C,0x18,0x00,0x00]
 fastMultiplexing(patternGreen,patternRed,patternYellow,50)
 
+continuing = True
+
 if len(sys.argv) > 2:
    ledColor = sys.argv[1]
    text = sys.argv[2]
 else:
-    print "Choose text color: 1 for red, 2 for green, 3 for yellow"
-    ledColor = sys.stdin.read() 
-    print "Enter text to display and then press (Ctrl-D)."
-    text = sys.stdin.read()
+    while (continuing == True):
+        print "Choose text color: 1 for red, 2 for green, 3 for yellow."
+        ledColor = sys.stdin.read() 
+        print "\nEnter text to display and then press (Ctrl-D)."
+        text = sys.stdin.read()
+        textScroll(ledColor,text)
+        print "\nDo you wish to enter more text?"
+        text = sys.stdin.read()
+        if (text == "yes\n" or text == "y\n"):
+            
+            continuing = True
+        else:
+            continuing = False
 
-textScroll(int(ledColor),text)
 print "Finished"
 
