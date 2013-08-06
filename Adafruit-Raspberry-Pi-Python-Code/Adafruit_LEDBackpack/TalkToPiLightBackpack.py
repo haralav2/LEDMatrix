5ccea4684aad3bcf19c3b3e22a7b970e96ab60b8
Veneta <haralav2@cs.man.ac.uk>
2013-08-06 06:11:07 +0000

    Simple text displaying program for the matrix

---
+++ Adafruit-Raspberry-Pi-Python-Code/Adafruit_LEDBackpack/TalkToPiLightBackpack.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import smbus
+import time
+import sys
+import font8x8
+import Adafruit_8x8Veni
+
+grid = Adafruit_8x8Veni.ColorEightByEight(address=0x70)
+
+colour = 1
+# Initialise matrix
+grid.clear()
+
+# Greet the user 
+greeting = "Hello!What's your name?\n"
+print greeting
+grid.textScroll(colour,greeting)
+
+name = sys.stdin.read()
+nameAndGreeting = "\nHello " + name + "I am PiLight!"
+print nameAndGreeting
+grid.textScroll(colour,nameAndGreeting)
+
+continuing = True
+
+if len(sys.argv) > 2:
+   ledColor = sys.argv[1]
+   text = sys.argv[2]
+else:
+    while (continuing == True):
+        print "\nChoose text color: 1 for red, 2 for green, 3 for yellow."
+        colour = sys.stdin.read() 
+        print "\nEnter text to display and then press (Ctrl-D)."
+        text = sys.stdin.read()
+        grid.textScroll(colour,text)
+        print "\nDo you wish to enter more text?"
+        text = sys.stdin.read()
+        if (text == "yes\n" or text == "y\n"):
+            
+            continuing = True
+        else:
+            continuing = False
+
+print "\nFinished"
+
